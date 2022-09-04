5790574d3b1ab1ca235c0e159d0f745b21f18141
venomDev <venomdev@gmail.com>
2022-09-04 16:54:09 +0930

    Create basic to do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+type ToDoList = string[];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [toDo, setToDo] = useState("");
+	const [toDoList, setToDoList] = useState<ToDoList>([]);
+
+	return (
+		<div className="App">
+			<h1>React To Do App</h1>
+			<div className="input-wrapper">
+				<input
+					type="text"
+					name="todo"
+					placeholder="Create a new todo"
+					onChange={(e) => {
+						setToDo(e.target.value);
+					}}
+					value={toDo}
+				/>
+				<button className="add-button" onClick={addToDo}>
+					Add
+				</button>
+				{toDoList?.length > 0 ? (
+					<ul className="todo-list">
+						{toDoList.map((item, index) => (
+							<li key={index}>
+								<div className="todo">
+									{item}
+									<button
+										className="delete-button"
+										onClick={() => deleteToDo(item)}
+									>
+										Delete
+									</button>
+								</div>
+							</li>
+						))}
+					</ul>
+				) : (
+					<div className="empty">
+						<p>No task found</p>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+
+	function addToDo() {
+		if (toDo !== "") {
+			console.info("add:", toDo);
+			setToDoList([...toDoList, toDo]);
+			setToDo("");
+		}
+	}
+
+	function deleteToDo(text: string) {
+		const newToDos = toDoList.filter((todo) => {
+			return todo != text;
+		});
+		setToDoList(newToDos);
+	}
 }
 
 export default App;
